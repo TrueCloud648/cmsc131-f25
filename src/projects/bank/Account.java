cae7098ecbab80be3de20857565f52528d679d5f
Matthew Pearson <mattyp0510@icloud.com>
2025-10-01 17:58:04 +0000

    Added data validation to Account class

--- src/projects/bank/Account.java
+++ src/projects/bank/Account.java
@@ -1,31 +1,41 @@
 package projects.bank;
 
 public class Account {
-    String ID; // TODO camelCase should use lowercase first letter
+    String id; // camelCase should use lowercase first letter
     String ownerName; // correct camelCase variable name
     double balance;
     AccountType type;
 
-    public Account(
-    String idValue,
-    String owner,
-    double bal,
-    AccountType typeValue
-    )  {
-        // TODO: Validate that String parameters are not null
-        ID = idValue;
+    public Account(String idValue, String owner, double bal, 
+    AccountType typeValue)  {
+        if (idValue == null) {
+            throw new IllegalArgumentException(
+                "idValue cannot be null."
+            );
+        }
+        if (owner == null) {
+            throw new IllegalArgumentException(
+                "owner cannot be null."
+            );    
+        }
+        if (typeValue == null) {
+            throw new IllegalArgumentException(
+                "typeValue cannot be null."
+            );
+        }
+        id = idValue;
         ownerName = owner;
         balance = bal;
         type = typeValue;
     }
 
-    public String getID() { return ID; }
+    public String getId() { return id; }
     public String getownerName() { return ownerName; }
     public double getbalance() { return balance; }
 
     // stripped-down version of 'equals' override
     // give it a similar sounding name
     public boolean sameAs(Account other) {
-        return this.getID().equals(other.getID());
+        return this.getId().equals(other.getId());
     }
 }
