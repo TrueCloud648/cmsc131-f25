02be12726f14e15774c1a6ef4bc4714777f22828
John M Dusel <jdusel@frederick.edu>
2025-10-11 08:11:10 -0400

    FBK: Bank class

--- src/projects/bank/Bank.java
+++ src/projects/bank/Bank.java
@@ -3,5 +3,6 @@ package projects.bank;
 public class Bank {
     Account[] accounts;
     // TODO add constructor and methods per specification in Course Notes
-    public Bank(String idValue, String owner)
+    public Bank(String idValue, String owner) {}
+    // TODO finish constructor and methods per course notes
 }
