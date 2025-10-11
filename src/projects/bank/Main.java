3ba3ce895f9dff34c464cf49d4ae64b45c2b5ea1
John M Dusel <jdusel@frederick.edu>
2025-10-11 08:14:19 -0400

    ENH: projects.bank: Add driver code
    Dusel will maintain this file as we finish future phases.
    
    Once your Account and Bank classes are updated, this program will run to create a file `phase1.log`

--- src/projects/bank/Main.java
+++ src/projects/bank/Main.java
@@ -1,3 +1,62 @@
-public class Main {
+package projects.bank;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        phase1();
+    }
+
+    public static void phase1() {
+        String logName = "phase1.log";
+        try {
+            FileWriter writer = new FileWriter(new File(logName));
+
+            Account acct = new Account(
+                "id1",
+                "Owner Name",
+                1.0,
+                AccountType.SAVINGS
+            );
+            
+            writer.write(
+                String.format(
+                    "Account setup: acct id=%s, owner=%s, balance=%f",
+                    acct.getId(),
+                    acct.getownerName(),
+                    acct.getbalance()
+                ) + System.lineSeparator()
+            );
+
+            Bank bank = new Bank();
+            int numAccounts0 = bank.getCount();
+            int findAcct0 = bank.find(acct.getId());
+
+            boolean addResult = bank.add(acct);
+            int numAccounts1 = bank.getCount();
+            int findAcct1 = bank.find(acct.getId());
+
+            writer.write(
+                String.format(
+                    "Bank init: getCount=%d, find=%d",
+                    numAccounts0, 
+                    findAcct0
+                ) + System.lineSeparator()
+            );
+
+            writer.write(
+                String.format(
+                    "After add: result=%b, getCount=%d, find=%d",
+                    addResult,
+                    numAccounts1, 
+                    findAcct1
+                ) + System.lineSeparator()
+            );
+
+            writer.close();
+        } catch (IOException e) { e.printStackTrace(); }
+
+    }
 }
