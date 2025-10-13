450b7cababd6c761c4586d3fe1e1413a43d6c967
Matthew Pearson <mattyp0510@icloud.com>
2025-10-13 19:24:53 +0000

    Added a bunch of test coverage, hoping it helps for submitting for phase 2

--- src/projects/bank/Bank.java
+++ src/projects/bank/Bank.java
@@ -1,8 +1,57 @@
 package projects.bank;
 
+import java.lang.reflect.Array;
+
 public class Bank {
-    Account[] accounts;
-    // TODO add constructor and methods per specification in Course Notes
-    public Bank(String idValue, String owner) {}
-    // TODO finish constructor and methods per course notes
+
+    private Account[] accounts;
+    private int idxNextAccount;
+    private final int newAccountsIncrement = 100;
+
+    public Bank() {
+        accounts = new Account[newAccountsIncrement];
+    }
+
+    public boolean add (Account acct) {
+        if (acct == null) {
+            throw new IllegalArgumentException(
+                "Account cannot be null."
+            );
+        }
+        if (find(acct.getID()) == -1) {
+            try {accounts[idxNextAccount] = acct;
+            } catch(ArrayIndexOutOfBoundsException e) {
+                Account[] accountsExtended = new Account[
+                    accounts.length + newAccountsIncrement
+                ];
+                for (int idx = 0; idx < accounts.length; idx++) {
+                    accountsExtended[idx] = accounts[idx];
+                }
+                accountsExtended[idxNextAccount] = acct;
+                accounts = accountsExtended;
+            }
+            idxNextAccount++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int find(String accountID) {
+        if (accountID == null) {
+            throw new IllegalArgumentException(
+                "AccountID cannot be null."
+            );
+        }
+        for (int idxAcct = 0; idxAcct < idxNextAccount; idxAcct++) {
+            if (accounts[idxAcct].getID().equals(accountID)) {
+                return idxAcct;
+            }
+        }
+        return -1;
+    }
+
+    public int getCount() {
+        return idxNextAccount;
+    }
 }
--- test/projects/bank/BankTest.java
+++ test/projects/bank/BankTest.java
@@ -1,8 +1,10 @@
 package projects.bank;
 
-import org.junit.Before;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import projects.bank.Bank;
 
 
 public class BankTest {
@@ -16,20 +18,88 @@ public class BankTest {
     }
 
     @Test
-    void testAddAccount() {
-        // add account that's absent from bank's accounts
-
-        // add account that's present in bank's accounts (no effect)
+    void testAddDataValidation() {
+        Exception e = assertThrows(
+        IllegalArgumentException.class,
+        () -> {bank.add(null);}
+        );
+        assertEquals("Account must not be null.", e.getMessage());
     }
 
     @Test
-    void testAddAccountOverflow() {}
+    void testAddAccount() {
+        // add account that's absent from bank's accounts
+        boolean addAccountResult = bank.add(acct);
+        assertEquals(
+            true,
+            addAccountResult,
+            "bank.add should return true."
+        );
+        assertEquals(
+            1,
+            bank.getCount(),
+            "bank.getCount should be 1."
+        );
+
+        // add account that's present in bank's accounts (no effect)
+        addAccountResult = bank.add(acct);
+        assertEquals(
+            false,
+            addAccountResult,
+            "bank.add should return false."
+        );
+        assertEquals(
+            1,
+            bank.getCount(),
+            "bank.getCount should still be 1."
+        );
+    }
+
+    @Test
+    void testAddAccountOverflow() {
+        for (int idx = 0; idx <= 100; idx++) {
+            Integer id = idx;
+            bank.add(
+                new Account(
+                    id.toString(),
+                    "Owner Name",
+                    1.0,
+                    AccountType.CHECKING
+                )
+            );
+        }
+
+        assertEquals(
+            101,
+            bank.getCount(),
+            "bank should hold 101 accounts."
+        );
+    }
+
+    @Test
+    void testFindDataValidation() {
+        Exception e = assertThrows(
+            IllegalArgumentException.class,
+            () -> {bank.find(null);}
+        );
+        assertEquals("accountID must not be null.", e.getMessage());
+    }
 
     @Test
     void testFind() {
         // add account and check its returned value
-
+        bank.add(acct);
+        assertEquals(
+            0,
+            bank.find(acct.getID()),
+            "acct should be at index 0."
+        );
         // check returned value for account not in bank
+        assertEquals(
+            -1,
+            bank.find("id1"),
+            "Result should be -1 when finding absent account."
+        );
     }
 
 } // end: class BankTest
